0e46ea82acb641db6e2e0b30130d38829b40188e
卢思侗 <499422444@qq.com>
2017-01-14 16:24:13 +0800

    test (#1)

--- src/scripts/static.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {renderToStaticMarkup} from 'react-dom/server';
-import {Router, browserHistory} from 'react-router';
-
-import routes from '~/routes';
-
-//export default renderToStaticMarkup(<Router history={browserHistory}>{routes}</Router>);
-export default routes;
